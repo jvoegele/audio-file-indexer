66a59a9d4e45f17195c857fed3c7830ad5aec46e
Jason Voegele <jason.voegele@gmail.com>
2015-08-15 13:28:00 -0400

    Initial commit of Leiningen project skeleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to audio-file-indexer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/audio_file_indexer/core.clj
@@ -0,0 +1,7 @@
+(ns audio-file-indexer.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
